102681a9b94f382475c3921a9ef085f4bd43589e
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2025-05-28 09:45:46 -0400

    Update reEnableblocked.js

--- js/reEnableblocked.js
+++ js/reEnableblocked.js
@@ -1,1 +1,2 @@
-function(){document.querySelectorAll('[style*="pointer-events: none"]').forEach(el=>{el.style/.pointerEvents='auto';el.style.opacity='1';});})()
+(function(){document.querySelectorAll('[style*="pointer-events: none"]').forEach(el=>{el.style/.pointerEvents='auto';el.style.opacity='1';});})()
+(function(){document.querySelectorAll('[disabled],[readonly]').forEach(el=>{el.removeAttribute('disabled');el.removeAttribute('readonly');});})();
